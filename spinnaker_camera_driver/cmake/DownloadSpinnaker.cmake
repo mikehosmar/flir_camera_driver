12504471ecf82c42cf9a5f45249dece9372d10fd
Mike Hosmar <mikehosmar@gmail.com>
2020-07-30 12:08:33 -0400

    move to python3 for download_spinnaker, fixes to where SDK is downloaded.

--- spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
+++ spinnaker_camera_driver/cmake/DownloadSpinnaker.cmake
@@ -10,12 +10,12 @@ function(download_spinnaker FLIR_LIB_VAR FLIR_INCLUDE_DIR_VAR)
 
   include(cmake/TargetArch.cmake)
   target_architecture(FLIR_ARCH)
-  set(FLIR_DIR ${CMAKE_CURRENT_BINARY_DIR}/usr/lib)
+  set(FLIR_DIR ${CMAKE_CURRENT_BINARY_DIR})
   set(DOWNLOAD_SCRIPT "${PROJECT_SOURCE_DIR}/cmake/download_spinnaker")
 
   message(STATUS "Running download_spinnaker script with arguments: ${FLIR_ARCH} ${FLIR_DIR} ${OS_CODE_NAME}")
   execute_process(
     COMMAND ${DOWNLOAD_SCRIPT} ${FLIR_ARCH} "${FLIR_DIR}" ${OS_CODE_NAME})
-  set(${FLIR_LIB_VAR} "${FLIR_DIR}/libSpinnaker.so" PARENT_SCOPE)
-  set(${FLIR_INCLUDE_DIR_VAR} "${CMAKE_CURRENT_BINARY_DIR}/usr/include/spinnaker" PARENT_SCOPE)
+  set(${FLIR_LIB_VAR} "${FLIR_DIR}/usr/lib/libSpinnaker.so" PARENT_SCOPE)
+  set(${FLIR_INCLUDE_DIR_VAR} "${FLIR_DIR}/usr/include/spinnaker" PARENT_SCOPE)
 endfunction()
